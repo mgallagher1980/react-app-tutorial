a5dc7ff57203a93219f7ebe945f36a8648a53f4a
Cheguegallagher <martin.gallagher@nice.org.uk>
2018-11-08 08:38:23 +0000

    adding tests to project

--- src/Kitten/Kitten.jsx
+++ src/Kitten/Kitten.jsx
@@ -1,13 +1,17 @@
 import React, { Component, Fragment } from 'react';
 
 export default class Kitten extends Component {
+  
+  cuteness() {
+    return 99;
+  }
+  
   render() {
     const kitten = this.props.kitten;
     const index = this.props.index;
 
     return (
       <Fragment>
-        <span>{index + 1}</span>
       <p>{kitten.title}</p>
       <img
        src={kitten.images.preview_gif.url} 
